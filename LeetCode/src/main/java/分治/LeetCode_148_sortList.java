e7b4da7ceafbd06a0f6e60ac5be685bf51affdc1
gyy <GYY123456>
2024-03-18 00:33:06 +0800

    add

--- LeetCode/src/main/java/分治/LeetCode_148_sortList.java
+++ LeetCode/src/main/java/分治/LeetCode_148_sortList.java
@@ -1,12 +1,35 @@
 package 分治;
 
- class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public static void printNodeList(ListNode head) {
+        if (head.next == null) {
+            System.out.println("head: " + head.val);
+        }
+        System.out.print("head: ");
+        while (head.next != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.print(head.val);
+        System.out.println(" \n");
+    }
+}
+
 public class LeetCode_148_sortList {
     public ListNode sortList(ListNode head) {
         // 1、递归结束条件
@@ -46,11 +69,11 @@ public class LeetCode_148_sortList {
 
     // 合并两个有序链表（21. 合并两个有序链表）
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode sentry = new ListNode(-1);
-        ListNode curr = sentry;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
 
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 curr.next = l1;
                 l1 = l1.next;
             } else {
@@ -62,6 +85,17 @@ public class LeetCode_148_sortList {
         }
 
         curr.next = l1 != null ? l1 : l2;
-        return sentry.next;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_148_sortList leetCode_148_sortList = new LeetCode_148_sortList();
+        ListNode root = new ListNode(4);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(1);
+        root.next.next.next = new ListNode(3);
+        ListNode.printNodeList(leetCode_148_sortList.sortList(root));
+//        root.next.next.next.next = new ListNode(5);
+//        ListNode.printNodeList(leetCode_148_sortList.sortList(root));
     }
 }
