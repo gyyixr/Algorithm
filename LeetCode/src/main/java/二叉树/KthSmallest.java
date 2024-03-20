afca65e4e35a32aafc083f4afc00c8e56fd9a254
gyy <GYY123456>
2024-03-21 01:25:42 +0800

    add

---
+++ LeetCode/src/main/java/二叉树/KthSmallest.java
@@ -0,0 +1,50 @@
+package 二叉树;
+
+//给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+public class KthSmallest {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int count = 0;
+
+    public TreeNode KthNode(TreeNode pRoot, int k) {
+        if (pRoot == null) {
+            return null;
+        }
+        TreeNode node = KthNode(pRoot.left, k);
+        if (node != null) {
+            return node;
+        }
+        count++;
+        if (count == k) {
+            return pRoot;
+        }
+        node = KthNode(pRoot.right, k);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        KthSmallest solution = new KthSmallest();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.left.right = new TreeNode(2);
+        System.out.println(solution.KthNode(root, 1).val);
+    }
+}
