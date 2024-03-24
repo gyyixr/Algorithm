acb6f24777162dac56f72b37632b4539952a1399
guoyuyang <guoyuyang@didiglobal.com>
2024-03-25 02:09:28 +0800

    add

--- LeetCode/src/main/java/回溯/LeetCode_93_restoreIpAddresses.java
+++ LeetCode/src/main/java/回溯/LeetCode_93_restoreIpAddresses.java
@@ -1,5 +1,6 @@
 package 回溯;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -48,64 +49,100 @@ public class LeetCode_93_restoreIpAddresses {
         }
     }
 
+    //====================================================================================
+    List<String> result = new ArrayList<>();
+
+    public List<String> restoreIpAddressesII(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        backTracking(sb, 0, 0);
+        return result;
+    }
+
+    private void backTracking(StringBuilder s, int startIndex, int dotCount) {
+        if (dotCount == 3) {
+            if (isValid(s, startIndex, s.length() - 1)) {
+                result.add(s.toString());
+            }
+            return;
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            if (isValid(s, startIndex, i)) {
+                s.insert(i + 1, '.');
+                backTracking(s, i + 2, dotCount + 1);
+                s.deleteCharAt(i + 1);
+            } else {
+                break;
+            }
+        }
+    }
+
+    //[start, end]
+    private boolean isValid(StringBuilder s, int start, int end) {
+        if (start > end) return false;
+        if (s.charAt(start) == '0' && start != end) return false;
+        return Integer.parseInt(s.substring(start, end + 1)) <= 255;
+    }
+
+
     public static void main(String[] args) {
         LeetCode_93_restoreIpAddresses leetCode_93_restoreIpAddresses = new LeetCode_93_restoreIpAddresses();
         System.out.println(leetCode_93_restoreIpAddresses.restoreIpAddresses("101023"));
+        System.out.println(leetCode_93_restoreIpAddresses.restoreIpAddressesII("0000"));
     }
 
 }
 
 /**
  * class Solution {
- *     List<String> result = new ArrayList<>();
- *
- *     public List<String> restoreIpAddresses(String s) {
- *         if (s.length() > 12) return result; // 算是剪枝了
- *         backTrack(s, 0, 0);
- *         return result;
- *     }
- *
- *     // startIndex: 搜索的起始位置， pointNum:添加逗点的数量
- *     private void backTrack(String s, int startIndex, int pointNum) {
- *         if (pointNum == 3) {// 逗点数量为3时，分隔结束
- *             // 判断第四段⼦字符串是否合法，如果合法就放进result中
- *             if (isValid(s,startIndex,s.length()-1)) {
- *                 result.add(s);
- *             }
- *             return;
- *         }
- *         for (int i = startIndex; i < s.length(); i++) {
- *             if (isValid(s, startIndex, i)) {
- *                 s = s.substring(0, i + 1) + "." + s.substring(i + 1);    //在str的后⾯插⼊⼀个逗点
- *                 pointNum++;
- *                 backTrack(s, i + 2, pointNum);// 插⼊逗点之后下⼀个⼦串的起始位置为i+2
- *                 pointNum--;// 回溯
- *                 s = s.substring(0, i + 1) + s.substring(i + 2);// 回溯删掉逗点
- *             } else {
- *                 break;
- *             }
- *         }
- *     }
- *
- *     // 判断字符串s在左闭⼜闭区间[start, end]所组成的数字是否合法
- *     private Boolean isValid(String s, int start, int end) {
- *         if (start > end) {
- *             return false;
- *         }
- *         if (s.charAt(start) == '0' && start != end) { // 0开头的数字不合法
- *             return false;
- *         }
- *         int num = 0;
- *         for (int i = start; i <= end; i++) {
- *             if (s.charAt(i) > '9' || s.charAt(i) < '0') { // 遇到⾮数字字符不合法
- *                 return false;
- *             }
- *             num = num * 10 + (s.charAt(i) - '0');
- *             if (num > 255) { // 如果⼤于255了不合法
- *                 return false;
- *             }
- *         }
- *         return true;
- *     }
+ * List<String> result = new ArrayList<>();
+ * <p>
+ * public List<String> restoreIpAddresses(String s) {
+ * if (s.length() > 12) return result; // 算是剪枝了
+ * backTrack(s, 0, 0);
+ * return result;
+ * }
+ * <p>
+ * // startIndex: 搜索的起始位置， pointNum:添加逗点的数量
+ * private void backTrack(String s, int startIndex, int pointNum) {
+ * if (pointNum == 3) {// 逗点数量为3时，分隔结束
+ * // 判断第四段⼦字符串是否合法，如果合法就放进result中
+ * if (isValid(s,startIndex,s.length()-1)) {
+ * result.add(s);
+ * }
+ * return;
+ * }
+ * for (int i = startIndex; i < s.length(); i++) {
+ * if (isValid(s, startIndex, i)) {
+ * s = s.substring(0, i + 1) + "." + s.substring(i + 1);    //在str的后⾯插⼊⼀个逗点
+ * pointNum++;
+ * backTrack(s, i + 2, pointNum);// 插⼊逗点之后下⼀个⼦串的起始位置为i+2
+ * pointNum--;// 回溯
+ * s = s.substring(0, i + 1) + s.substring(i + 2);// 回溯删掉逗点
+ * } else {
+ * break;
+ * }
+ * }
+ * }
+ * <p>
+ * // 判断字符串s在左闭⼜闭区间[start, end]所组成的数字是否合法
+ * private Boolean isValid(String s, int start, int end) {
+ * if (start > end) {
+ * return false;
+ * }
+ * if (s.charAt(start) == '0' && start != end) { // 0开头的数字不合法
+ * return false;
+ * }
+ * int num = 0;
+ * for (int i = start; i <= end; i++) {
+ * if (s.charAt(i) > '9' || s.charAt(i) < '0') { // 遇到⾮数字字符不合法
+ * return false;
+ * }
+ * num = num * 10 + (s.charAt(i) - '0');
+ * if (num > 255) { // 如果⼤于255了不合法
+ * return false;
+ * }
+ * }
+ * return true;
+ * }
  * }
  */
