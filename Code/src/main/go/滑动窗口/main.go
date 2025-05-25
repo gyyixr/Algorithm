b8d59946204aaacfd4675491d168452820b7ced9
gyy <GYY123456>
2025-05-26 00:58:33 +0800

    update

---
+++ Code/src/main/go/滑动窗口/main.go
@@ -0,0 +1,45 @@
+package main
+
+/**
+ 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+示例：
+输入：s = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+*/
+
+func minSubArrayLen(target int, nums []int) int {
+	i := 0
+	l := len(nums)  // 数组长度
+	sum := 0        // 子数组之和
+	result := l + 1 // 初始化返回长度为l+1，目的是为了判断“不存在符合条件的子数组，返回0”的情况
+	for j := 0; j < l; j++ {
+		sum += nums[j]
+		for sum >= target {
+			subLength := j - i + 1
+			if subLength < result {
+				result = subLength
+			}
+			sum -= nums[i]
+			i++
+		}
+	}
+	if result == l+1 {
+		return 0
+	} else {
+		return result
+	}
+}
+
+func main() {
+	// 示例
+	s := 7
+	nums := []int{2, 3, 1, 2, 4, 3}
+	result := minSubArrayLen(s, nums)
+	println(result) // 输出：2
+
+	s2 := 11
+	nums2 := []int{1, 2, 3, 4, 5}
+	result2 := minSubArrayLen(s2, nums2)
+	println(result2) // 输出：3
+}
